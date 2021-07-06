6735b8969349f5739dbf942da88c569942186955
Leonardo Balsalobre <oliveirabalsa2@gmail.com>
2021-07-06 01:58:27 -0300

    refactor: remove http boilerplate

--- src/infra/integration/axios/config.ts
+++ src/infra/integration/axios/config.ts
@@ -3,8 +3,3 @@ import Axios from "axios";
 export const http = Axios.create({
   baseURL: "https://fit-web-scraping-challenge.herokuapp.com/",
 });
-
-// export const httpDonation = Axios.create({
-//   baseURL:
-//     "http://newschooldonormetric-env.eba-mxmw3egz.us-east-2.elasticbeanstalk.com",
-// });
