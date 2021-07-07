b6fdb75664e3e442e53df3157afe27aa78706d7a
Leonardo Balsalobre <oliveirabalsa2@gmail.com>
2021-07-07 11:51:01 -0300

    feat: add axios retry

--- src/infra/integration/axios/config.ts
+++ src/infra/integration/axios/config.ts
@@ -1,5 +1,9 @@
 import Axios from "axios";
+import axiosRetry from "axios-retry";
 
 export const http = Axios.create({
   baseURL: "https://fit-web-scraping-challenge.herokuapp.com/",
 });
+
+axiosRetry(http, { retries: 3 });
+
