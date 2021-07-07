019e80478cd0f21dc03dcc5750ce3ac9cbadcc48
Leonardo Balsalobre <oliveirabalsa2@gmail.com>
2021-07-07 11:54:10 -0300

    feat: add cache validation in makelogin usecase

--- src/usecase/implementation/MakeLoginUseCaseImpl.ts
+++ src/usecase/implementation/MakeLoginUseCaseImpl.ts
@@ -2,14 +2,21 @@ import AuthenticateIntegrationImpl from "../../infra/integration/playwright/impl
 import IPage from "../../infra/integration/playwright/interface/IPage";
 import ICredentials from "../dto/ICredentials";
 import IMakeLoginUseCase from "../interface/IMakeLoginUseCase";
+import writeFile from "../../web/helper/WriteToFileHelper";
+import getCached from "../../web/helper/GetCachedPageHelper";
 
 export default class MakeLoginUseCaseImpl implements IMakeLoginUseCase {
   async execute(credentials: ICredentials): Promise<IPage> {
-    const authenticateIntegration = new AuthenticateIntegrationImpl();
-    const pageData = await authenticateIntegration.execute(
-      credentials
-    );
+    const cachedPageData = await getCached.execute("page-data", "json");
+    if (cachedPageData) {
+      return JSON.parse(cachedPageData);
+    }
 
-    return pageData
+    const authenticateIntegration = new AuthenticateIntegrationImpl();
+    const pageData = await authenticateIntegration.execute(credentials);
+
+    writeFile.execute(JSON.stringify(pageData), "page-data", "json");
+
+    return pageData;
   }
 }
